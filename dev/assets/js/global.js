982644fd97994a679e945c34f7965736e68ec13d
Rostislav <suslovros@gmail.com>
2022-10-28 16:54:50 +0300

    first

---
+++ dev/assets/js/global.js
@@ -0,0 +1,69 @@
+jQuery(document).ready(function($) {
+    $(".fancybox").fancybox();
+    $('.nice_Select').niceSelect();
+    
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50) {
+            $('.scrolltop:hidden').stop(true, true).fadeIn();
+        } else {
+            $('.scrolltop').stop(true, true).fadeOut();
+        }
+    });
+
+    $(function() {
+        $(".muve-top").click(function() {
+            $("html,body").animate({
+                scrollTop: $(".thetop").offset().top
+            }, "1000");
+            return false
+        })
+    })
+
+    if ($('.post-item img').length || $('.seo-block img').length) {
+        $('.post-item img, .seo-block img').each(function() {
+            var imgThis = $(this);
+            if (!imgThis.hasClass("nofancy")) {
+                if (imgThis.closest('a')) {
+                    imgThis.closest('a').attr({
+                        'data-fancybox': 'content-group',
+                        //'href': $(this).attr('src'),
+                    });
+                };
+            };
+        });
+    };
+    
+    if($('.post-item iframe').length){
+        $('.post-item iframe').closest('p').addClass('iframe-box')
+    }
+
+       
+    var nav = $('.language-title');
+    var selection = $('.language-list');
+    var select = selection.find('li');
+
+    nav.click(function(event) {
+        if (nav.hasClass('active')) {
+            nav.removeClass('active');
+            selection.stop().slideUp(200);
+        } else {
+            nav.addClass('active');
+            selection.stop().slideDown(200);
+        }
+        event.preventDefault();
+    });
+
+    select.click(function(event) {
+        select.removeClass('active');
+        $(this).addClass('active');
+        var $langAbr = $(this).text();
+        // var dataLang = $(this).attr('data-lang')
+        nav.text($langAbr);
+        // nav.prop('data-lang', $langAbr);
+
+        
+        nav.trigger('click');
+        //alert($lang);
+        // alert ("location.href = 'index.php?lang=" + $(this).attr('data-value'));
+    });
+})
